3321d199b1f7c85fbf82339a4a906ab15d70c96e
DukaSobaka <133567187+PoedatelPelmeniv@users.noreply.github.com>
2023-06-18 12:53:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import play
+from play.play import Sprite, text
+
 
 play.set_backdrop('light blue')
 
@@ -13,7 +15,7 @@ def start():
     speech = play.new_text(words='Що я тут роблю?', x=0, y=-250)
 @play.repeat_forever # Повторювати завжди (ігровий цикл)
 async def do():
-    if play.key_is_pressed("п") or play.key_is_pressed("П"):
+    if play.key_is_pressed("п") or play.key_is_pressed("П") or play.key_is_pressed("g") or play.key_is_pressed("G"):
         player.image = 'hello.jpg'
         speech.words = 'Здоров був!'
         await play.timer(3.0)
@@ -22,7 +24,7 @@ async def do():
         await play.timer(2.0)
         player.image = 'NO2.jpg'
         speech.words = 'Може я краще піду!?'
-    if play.key_is_pressed("о") or play.key_is_pressed("О"):
+    if play.key_is_pressed("о") or play.key_is_pressed("О") or play.key_is_pressed("j") or play.key_is_pressed("J"):
         player.image = 'obnimashki.jpg'
         speech.words = 'Задушиш..'
         await play.timer(3.0) 
@@ -31,7 +33,7 @@ async def do():
         await play.timer(2.0)
         player.image = 'NO2.jpg'   
         speech.words = "Ти занадто нав'язливий"      
-    if play.key_is_pressed("ж") or play.key_is_pressed("Ж"):
+    if play.key_is_pressed("ж") or play.key_is_pressed("Ж") or play.key_is_pressed(";") or play.key_is_pressed(":"):
         player.image = 'Laugh.jpg'
         speech.words = 'лол, ахахх..'
         await play.timer(3.0)
@@ -40,7 +42,7 @@ async def do():
         await play.timer(5.0)
         player.image = 'NO2.jpg'   
         speech.words = 'Мені стає нудно...'
-    if play.key_is_pressed("с") or play.key_is_pressed("С"):
+    if play.key_is_pressed("с") or play.key_is_pressed("С") or play.key_is_pressed("c") or play.key_is_pressed("C"):
         player.image = 'sleep.jpg'
         speech.words = 'ZzZz..'
         await play.timer(3.0)
@@ -48,8 +50,8 @@ async def do():
         speech.words = 'Я прокинувся'
         await play.timer(5.0)
         player.image = 'NO2.jpg'   
-        speech.words = 'Дай встати! Свободу мишам!'
-    if play.key_is_pressed("ф") or play.key_is_pressed("Ф"):
+        speech.words = 'Дай встати!'
+    if play.key_is_pressed("ф") or play.key_is_pressed("Ф") or play.key_is_pressed("a") or play.key_is_pressed("A"):
         player.image = 'Focus.jpg'
         speech.words = 'Ти що чаклун?'
         await play.timer(3.0)
@@ -58,7 +60,7 @@ async def do():
         await play.timer(5.0)
         player.image = 'NO2.jpg'   
         speech.words = 'Та ти не чаклун.'
-    if play.key_is_pressed("і") or play.key_is_pressed("І"):
+    if play.key_is_pressed("і") or play.key_is_pressed("І") or play.key_is_pressed("s") or play.key_is_pressed("S"):
         player.image = 'Food.jpg'
         speech.words = 'Дякую, дуже смачно!'
         await play.timer(3.0)
@@ -67,14 +69,20 @@ async def do():
         await play.timer(5.0)
         player.image = 'NO2.jpg'   
         speech.words = 'Мене зараз стошнить'
-    if play.key_is_pressed("space"):
+    if play.key_is_pressed("space"):   
         player.image = 'Bye.jpg'
         speech.words = "Бувай, приходь ще!"
-        await play.timer(10.0)
+        await play.timer(5.0)
         player.image = 'NO.jpg'   
         speech.words = 'Давай вже йди, не мозоль очі.'
+        await play.timer (2.0)
+        quit()
+    if play.key_is_pressed("з") or play.key_is_pressed("З") or play.key_is_pressed("p") or play.key_is_pressed("P"):
+        play.set_backdrop('dark red')
+        player.image = 'pashalka.jpg'
+        speech.words = '!??*"!(№?('
+        await play.timer (2.0)
         quit()
     pass
-
 
 play.start_program() # Запуск програми
